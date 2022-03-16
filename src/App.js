f83978264abce83da23a7cc1daec823f4546f835
alestojanovic <alestojanovic@ebay.com>
2022-03-16 13:33:48 +0100

    feat: added 404 page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AboutPage from "./pages/about/AboutPage";
 import Header from "./components/common/Header";
+import PageNotFound from "./components/PageNotFound";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return <h1>Ooops! That page is nowhere to be seen.</h1>;
+};
+
+export default PageNotFound;
