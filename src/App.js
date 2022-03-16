aef396341f70b73990ceb66be4ddd9133a65b215
alestojanovic <alestojanovic@ebay.com>
2022-03-16 13:27:36 +0100

    feat: added basic navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AboutPage from "./pages/about/AboutPage";
+import Header from "./components/common/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const activeStyle = { color: "#f15b2a" };
+  return (
+    <nav>
+      <NavLink to="/" className="text-style" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" className="text-style" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Header;
