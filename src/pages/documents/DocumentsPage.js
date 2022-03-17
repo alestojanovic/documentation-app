4f5588a1c36e1e82213f2b87f4f0ccfff9936db9
alestojanovic <alestojanovic@ebay.com>
2022-03-17 11:54:55 +0100

    feat: created input form

--- src/pages/documents/DocumentsPage.js
+++ src/pages/documents/DocumentsPage.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DocumentsPage.css";
 
 const DocumentsPage = () => {
+  const [document, setDocument] = useState({ title: "" });
+
+  const handleChange = (event) => {
+    const document = { ...document, title: event.target.value };
+    setDocument(document);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(document.title);
+  };
+
   return (
-    <div>
-      <h2>Documents Page</h2>
-      <p>We will display documentation here.</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Documents</h2>
+      <label className="input-label">Add Documents</label>
+      <span className="input-fields">
+        <input
+          className="input-field"
+          type="text"
+          onChange={handleChange}
+          value={document.title}
+        />
+        <input className="submit-button" type="submit" value="Save" />
+      </span>
+    </form>
   );
 };
 
