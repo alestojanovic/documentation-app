d7528acfde6a7fafb16f556a517a06831698e5ef
alestojanovic <alestojanovic@ebay.com>
2022-04-14 12:19:24 +0200

    feat: small color refactor

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
-  return <h1>Ooops! That page is nowhere to be seen.</h1>;
+  return (
+    <h1 className="gradient-text">Ooops! That page is nowhere to be seen.</h1>
+  );
 };
 
 export default PageNotFound;
